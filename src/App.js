4e42a08163eb551fec18cb385c8644933c83b1bc
jassem-lab <jassemgaaloul123@gmail.com>
2021-03-30 22:12:05 +0100

    add sphere

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import { Canvas } from 'react-three-fiber'
-import '/.App.css'
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import "/.App.css";
+
+function Sphere() {
+  return (
+    <mesh visible positions={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        // envMap={cubeCamera.renderTarget.texture}
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <Canvas>
-     
+      <Sphere />
     </Canvas>
   );
 }
